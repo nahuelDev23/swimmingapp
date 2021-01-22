d62352e752f9a6c1075917c65389fdb926dee9d9
nahueldev23 <nahuel.dev.23@gmail.com>
2021-01-22 10:01:39 -0300

    validaciones en la vista show de competencias

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -15,32 +15,32 @@ class SerieController extends Controller
 
     public function create(Competencia $competencia)
     {
-        $pruebas = Prueba::pluck('nombre_prueba','id');
-        return view('series/create',[
+        $pruebas = Prueba::pluck('nombre_prueba', 'id');
+        return view('series/create', [
             'pruebas' => $pruebas,
-            'competencia_id'=>$competencia->id,
-            
+            'competencia_id' => $competencia->id,
+
         ]);
     }
     public function store(Request $request)
     {
-        
+
         $serie = new Serie;
-        $serie->nombre_serie=$request->nombre_serie;
-        $serie->prueba_id=$request->prueba_id;
-        $serie->competencia_id=$request->competencia_id;
+        $serie->nombre_serie = $request->nombre_serie;
+        $serie->prueba_id = $request->prueba_id;
+        $serie->competencia_id = $request->competencia_id;
         $serie->save();
 
-        return redirect('competencias/'.$request->competencia_id);
+        return redirect('competencias/' . $request->competencia_id);
     }
 
     public function show(Serie $serie)
     {
-        
-       $cancheo = Cancheo::where('serie_id',$serie->id)->with('competidor')->orderBy('carril','asc')->get();
-        return view('series/show',[
+
+        $cancheo = Cancheo::where('serie_id', $serie->id)->with('competidor')->orderBy('carril', 'asc')->get();
+        return view('series/show', [
             'serie' => $serie,
-            'cancheo'=>$cancheo,
+            'cancheo' => $cancheo,
         ]);
     }
 
@@ -58,4 +58,3 @@ class SerieController extends Controller
     //     return $partition;
     // }
 }
-
