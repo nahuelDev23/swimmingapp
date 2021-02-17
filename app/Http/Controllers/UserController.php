6402f8e9a1bd1a9795a920389c4710af3f882d0c
nahueldev23 <nahuel.dev.23@gmail.com>
2021-02-17 12:31:59 -0300

    .env default

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -122,6 +122,7 @@ class UserController extends Controller
     }
 
     public function reset_password(Request $request){
+        dd($request);
         $this->validate($request, [
             'password' => 'required|confirmed|min:6',
         ]);
